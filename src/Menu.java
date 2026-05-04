ba9d512fb3b7ded097ef77405c85bea4e13e92d3
anirudhchakraborty <preetiujjal@gmail.com>
2026-05-05 03:00:40 +0530

    Start Menu - Method Updated

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Menu {
+    void StartMenu() {
+        System.out.println("Welcome to the Math Sprint Tester");
+        System.out.println("We are her to test your skills");
+        System.out.println("Please Chose a difficulty from 1 to 10");
+    }
 }
