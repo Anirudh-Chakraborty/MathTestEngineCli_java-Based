c552eea4ac9d9a901771728a5cc96993f0b3edba
anirudhchakraborty <preetiujjal@gmail.com>
2026-05-07 01:15:06 +0530

    Math Equation Constructor class created

--- src/MathEquation.java
+++ src/MathEquation.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 public class MathEquation {
     int difficulty;
     int numbeOfQuestion;
 
+    MathEquation(int difficulty, int numbeOfQuestion) {
+        this.difficulty = difficulty;
+        this.numbeOfQuestion = numbeOfQuestion;
+    }
+
+    List<Integer> generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < difficulty + 2; i++) {
+            numbers.add(caller.rand.nextInt(10*i));
+        }
+        return numbers;
+    }
 
 }
