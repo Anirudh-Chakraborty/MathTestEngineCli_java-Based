53eecde2ad1ba70a866ed441a03d9c98d810f351
anirudhchakraborty <preetiujjal@gmail.com>
2026-05-14 18:29:24 +0530

    Added Comments to the Equation class and also added the LIFOtoFIFO method so hat the stack is in Right order

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -4,23 +4,34 @@ public class EquationSolver {
     List<Object> equationList;
 
     EquationSolver(List<Object> equations) {
+    //this receives the list of equation
         this.equationList = equations;
     }
 
     void equationSender(List<List<Object>> equationList) {
         for(int i = 0; i < equationList.size(); i++) {
+            // from here we send the equations one by one
             stackMaker(equationList.get(i));
         }
     }
 
     void stackMaker(List<Object> equations) {
+        //this turns the equation into stack one by one
+        //into Lifo
         Stack<Object> stack = new Stack<>();
         for (int i = 0; i < equations.size(); i++) {
             stack.push(equations.get(i));
         }
+        LIFOtoFIFI(stack);
+    } 
+
+    void LIFOtoFIFI(Stack<Object> first) {
+    Stack<Object> stack = new Stack<>();
+        while (!first.empty()) {
+            stack.push(first.pop());
+        }
         solver(stack);
     }
-
     void solver(Stack<Object> stack){
 
     }
