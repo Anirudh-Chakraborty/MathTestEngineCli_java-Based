c683c70578c5831a818986625d00b750275e2f82
anirudhchakraborty <preetiujjal@gmail.com>
2026-05-18 03:08:16 +0530

    a new method added in menu when the user gives wrong answer

--- src/Menu.java
+++ src/Menu.java
@@ -7,4 +7,24 @@ public class Menu {
         System.out.println("Please Chose a difficulty from 1 to 10");
         System.out.println("And How many number of Questions you want");
     }
+
+//    void wrongAnswer(int currentScore, int percentage){
+    void wrongAnswer(){
+        System.out.println("Wrong answer.");
+        System.out.println("Try Again");
+        System.out.println("If You Wish to Quit press Q");
+        //for later us
+//        System.out.println("Your Current Score "+ currentScore);
+//        System.out.println("Your Percentage "+ percentage);
+    }
+
+//    void correctAnswer(int currentScore, int percentage){
+        void correctAnswer(){
+        System.out.println("Correct answer.");
+        System.out.println("Lets Get to the Next Question");
+        System.out.println("If You Wish to Quit press Q");
+//        for later use
+//        System.out.println("Your Current Score: " + currentScore);
+//        System.out.println("Your Percentage: " + percentage);
+    }
 }
