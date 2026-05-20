49045f115a9f7fb55318e5c9cae18d274d21f883
anirudhchakraborty <preetiujjal@gmail.com>
2026-05-21 03:26:49 +0530

    The User answers method now prints the Questions with proper indentation

--- src/VerifyAnswer.java
+++ src/VerifyAnswer.java
@@ -17,4 +17,21 @@ public class VerifyAnswer {
     }
 
 
+    void UserAnswer(List<Object>questions, List<Integer> answers){
+        //to check for each question
+        for(int i=0;i<questions.size();i++){
+            boolean next = false;
+            while(next == false) {
+
+                display((List<Object>) questions.get(i));
+                int answer = caller.sc.nextInt();
+
+                if (answer == answers.get(i)) {
+
+                }
+            }
+
+            System.out.println();
+        }
+    }
 }
